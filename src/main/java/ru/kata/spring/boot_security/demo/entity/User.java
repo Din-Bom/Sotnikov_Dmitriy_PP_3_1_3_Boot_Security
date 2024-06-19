abce8d435fed75751e214ebe61e7c6a9a34d7797
Дмитрий Сотников <DIMA10299@mail.ru>
2024-06-19 19:39:20 +0300

    Пока работает

--- src/main/java/ru/kata/spring/boot_security/demo/entity/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/entity/User.java
@@ -1,18 +1,26 @@
 package ru.kata.spring.boot_security.demo.entity;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,7 +33,8 @@ public class User {
 
     private String password;
 
-    /*private Set<Role> roleSet;*/
+    @ManyToMany(fetch = FetchType.LAZY)
+    private Set<Role> roleSet;
 
     public User() {
     }
@@ -38,11 +47,46 @@ public class User {
         this.id = id;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public @Size(min = 2, max = 30, message = "The name must not be less than 2 or more than 30 characters") @NotEmpty(message = "The field should not be empty") String getUsername() {
         return username;
     }
 
     public void setUsername(@Size(min = 2, max = 30, message = "The name must not be less than 2 or more than 30 characters") @NotEmpty(message = "The field should not be empty") String username) {
         this.username = username;
+    }
+
+    /*__________________________________________________________________________________________*/
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
